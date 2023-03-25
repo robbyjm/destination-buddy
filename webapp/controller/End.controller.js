0bf223a330c90a023effe534cb6fdfb525f2810c
Robby Manis <robbymanis@tutanota.com>
2023-03-24 21:31:11 -0400

    Adds destination section

--- webapp/controller/End.controller.js
+++ webapp/controller/End.controller.js
@@ -1,18 +1,18 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"../model/formatter"
-], function(Controller, formatter) {
-	"use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "../model/formatter"],
+  function (Controller, formatter) {
+    "use strict";
 
-	return Controller.extend("sap.ui.demo.basicTemplate.controller.End", {
+    return Controller.extend("sap.ui.demo.basicTemplate.controller.End", {
+      formatter: formatter,
 
-		formatter: formatter,
-
-		onInit: function () {
-			const oView = this.getView();
-			const oModel = this.getOwnerComponent().getModel()
-			const oCodeEditor = oView.byId('codeEditor')
-			oCodeEditor.setValue(oModel.getJSON())
-		}
-	});
-});
+      onInit: function () {
+        const oView = this.getView();
+        const oModel = this.getOwnerComponent().getModel();
+        const oCodeEditor = oView.byId("codeEditor");
+        const oCodeEditor2 = this.byId("codeEditor");
+        //oCodeEditor.setValue(oModel.getJSON());
+      },
+    });
+  }
+);
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,6 +1,18 @@
 sap.ui.define([], function () {
 	"use strict";
 	return {
-
+		color: function (sColor) {
+			switch (sColor) {
+				case 'NoAuthentication':
+					return 'Error'
+				case 'BasicAuthentication':
+					return 'Warning'
+				default:
+					return 'Information'
+			}
+		}
 	};
 });
+
+//buddy/destination-configuration/v1/subaccountDestinations
+//user-api/attributes
